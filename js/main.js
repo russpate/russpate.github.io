52e5e8b8ef63dadd322694d89b23b455bdb02048
Russell Pate <russ@russpate.com>
2016-04-26 14:06:46 -0400

    preparing to transition to templates and slightly modifying layout

--- js/main.js
+++ js/main.js
@@ -7,7 +7,11 @@ var page = {
     page.events();
   },
   events: function(){
-    
+    // $('.portfolio-item-wrap').hover(page.addPortIMG);
   },
-
+  // addPortIMG: function (event) {
+  //   $('.port-img').fadeIn().toggleClass('hidden');
+  //   $('.port-img-caption').fadeIn().toggleClass('hidden');
+  //   console.log("hovered")
+  // },
 }
